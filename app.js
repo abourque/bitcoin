cc8283547ea8fccf962dfff4e097b72d781e6d0a
Art B <rxarthurb@hotmail.com>
2019-12-21 05:49:48 -0500

    make first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.listen(3000, ()=>{
+    console.log("listening on port 3000");
+});
