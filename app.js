871c7aa0088605f1963be324e9c45e5dae6c1f1b
Art B <rxarthurb@hotmail.com>
2019-12-21 20:54:58 -0500

    make functional bitcoin quote page

--- app.js
+++ app.js
@@ -6,6 +6,31 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname +"/quote.html");
+    //res.end;
+});
+
+app.post("/", (req,res)=>{
+    var crypto = req.body.crypto;
+    var fiat = req.body.fiat;
+    var myurl = "https://apiv2.bitcoinaverage.com/indices/global/ticker/" + crypto + fiat;
+    //console.log(myurl);
+    //res.send(price);
+    
+    request(myurl, function(error, response, body){
+    
+        console.log("error : ", error);
+        console.log("status code: ", response && response.statusCode);
+        var json = JSON.parse(body);
+        var price = json.last;
+        //console.log(json.last);
+        res.send("<h1>The crypto ratio is " + price + "</h1>");
+        
+        });
+    
+});
+
 app.listen(3000, ()=>{
     console.log("listening on port 3000");
 });
